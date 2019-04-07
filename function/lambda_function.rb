568481274987df1b97571d58487013e1885c02e3
Fernando W. Bona <bonafernando@gmail.com>
2019-04-06 22:14:04 -0300

    k.i.s. using transform_keys

--- function/lambda_function.rb
+++ function/lambda_function.rb
@@ -2,7 +2,7 @@ require 'json'
 require 'rest_client'
 
 def lambda_handler(event:, context:)
-  request_params = Hash[event.map{|(k,v)| [k.to_sym,v]}]
+  request_params = event.transform_keys(&:to_sym)
   response = RestClient::Request.execute(request_params)
   result = JSON.parse(response.body)
   { statusCode: 200, body: result }
